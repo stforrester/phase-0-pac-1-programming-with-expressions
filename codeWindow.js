24d866926a128b1ce1287c0422ddd914a4e996bf
Stuart Forrester <stforrester@gmail.com>
2024-10-22 14:23:01 -0500

    updates codeWindow.js to v4 or whatever

--- codeWindow.js
+++ codeWindow.js
@@ -1,10 +1,14 @@
-// Input values: we could easily imagine asking a user for these values.
-const name = "Your name here";
-const probabilityOfRain = 0.48;
-const temperatureInC = 20;
+const name = "Spinach the Shiba";
+const probabilityOfRain = 0.2;
+const temperatureInC = 26;
 
-// Create our message
-const message = `Hello, ${name}, with a rain chance of ${probabilityOfRain * 100}% and a temperature of ${temperatureInC}C we recommend that you ` + (probabilityOfRain > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
-`${temperatureInC >= 26 ? ' and watch out for heatstroke.' : ' and bask in this fine weather.'}`;
+const likelyToRain = probabilityOfRain > 0.3;
+const sunIsDangerous = temperatureInC >= 26;
+const rainPercentage = probabilityOfRain * 100;
+
+const rainAdvice = likelyToRain ? "take an umbrella" : "enjoy this rain-free day";
+const sunAdvice = sunIsDangerous ? " and watch out for heatstroke" : " and bask in this fine weather";
+
+const message = `Hello, ${name}, with a rain chance of ${rainPercentage}% and a temperature of ${temperatureInC}C we recommend that you ` + rainAdvice + sunAdvice;
 
 console.log(message);
