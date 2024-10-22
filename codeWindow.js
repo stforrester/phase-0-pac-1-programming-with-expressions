2a4725552e355b7d619d95ea469ee37da92239da
Stuart Forrester <stforrester@gmail.com>
2024-10-22 14:30:11 -0500

    updates codeWindow.js penultimately

--- codeWindow.js
+++ codeWindow.js
@@ -4,11 +4,11 @@ const temperatureInC = 26;
 
 const likelyToRain = probabilityOfRain > 0.3;
 const sunIsDangerous = temperatureInC >= 26;
+
 const rainPercentage = probabilityOfRain * 100;
-
 const rainAdvice = likelyToRain ? "take an umbrella" : "enjoy this rain-free day";
-const sunAdvice = sunIsDangerous ? " and watch out for heatstroke" : " and bask in this fine weather";
+const sunAdvice = sunIsDangerous ? "watch out for heatstroke" : "bask in this fine weather";
 
-const message = `Hello, ${name}, with a rain chance of ${rainPercentage}% and a temperature of ${temperatureInC}C we recommend that you ` + rainAdvice + sunAdvice;
+const message = `Hello, ${name}, with a rain chance of ${rainPercentage}% and a temperature of ${temperatureInC}C we recommend that you ${rainAdvice} and ${sunAdvice}.`;
 
 console.log(message);
