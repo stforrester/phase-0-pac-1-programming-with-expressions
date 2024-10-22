d6aa1bb12a1529ca9677b683fd6f9a833eca75aa
Stuart Forrester <stforrester@gmail.com>
2024-10-22 14:33:04 -0500

    updates codeWindow.js finally

--- codeWindow.js
+++ codeWindow.js
@@ -1,14 +1,20 @@
-const name = "Spinach the Shiba";
-const probabilityOfRain = 0.2;
-const temperatureInC = 26;
+// const name = "Spinach the Shiba";
+// const probabilityOfRain = 0.2;
+// const temperatureInC = 26;
 
-const likelyToRain = probabilityOfRain > 0.3;
-const sunIsDangerous = temperatureInC >= 26;
+// const likelyToRain = probabilityOfRain > 0.3;
+// const sunIsDangerous = temperatureInC >= 26;
 
-const rainPercentage = probabilityOfRain * 100;
-const rainAdvice = likelyToRain ? "take an umbrella" : "enjoy this rain-free day";
-const sunAdvice = sunIsDangerous ? "watch out for heatstroke" : "bask in this fine weather";
+// const rainPercentage = probabilityOfRain * 100;
+// const rainAdvice = likelyToRain ? "take an umbrella" : "enjoy this rain-free day";
+// const sunAdvice = sunIsDangerous ? "watch out for heatstroke" : "bask in this fine weather";
 
-const message = `Hello, ${name}, with a rain chance of ${rainPercentage}% and a temperature of ${temperatureInC}C we recommend that you ${rainAdvice} and ${sunAdvice}.`;
+// const message = `Hello, ${name}, with a rain chance of ${rainPercentage}% and a temperature of ${temperatureInC}C we recommend that you ${rainAdvice} and ${sunAdvice}.`;
 
-console.log(message);
+// console.log(message);
+
+const first = 2;
+const second = 1;
+const problem = 99;
+const luckyNumber = first > second ? (problem - 1) / 2 : problem / 3;
+luckyNumber; //=> ??? (Test it out yourself!)
