b73d79eb912eb75dcc6353e6c3b35e9b78c35480
Stuart Forrester <stforrester@gmail.com>
2024-10-22 13:46:36 -0500

    updates codeWindow.js for canvas exercise 'Write a Program with Conditional Logic Using Expressions'

--- codeWindow.js
+++ codeWindow.js
@@ -1,2 +1,10 @@
-const sum = 1 + 1;
-console.log(sum);
+// Input values: we could easily imagine asking a user for these values.
+const name = "Your name here";
+const probabilityOfRain = 0.2;
+const temperatureInC = 26;
+
+// Create our message
+const message = `Hello, ${name}, with a rain chance of ${probabilityOfRain * 100}% and a temperature of ${temperatureInC}C we recommend that you ` + (probabilityOfRain > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
+`${temperatureInC >= 26 ? ' and watch out for heatstroke.' : ' and bask in this fine weather.'}`;
+
+console.log(message);
